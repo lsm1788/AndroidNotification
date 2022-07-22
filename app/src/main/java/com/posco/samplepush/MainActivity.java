677373fced18ce5daca09ac25a542d9f05629ba5
이승목 <lsm17888@naver.com>
2022-07-22 10:41:36 +0900

    Notification Test

---
+++ app/src/main/java/com/posco/samplepush/MainActivity.java
@@ -0,0 +1,223 @@
+package com.posco.samplepush;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // Channel에 대한 id 생성
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    // Channel을 생성 및 전달해 줄 수 있는 Manager 생성
+    private NotificationManager mNotificationManager;
+
+    // Notification에 대한 ID 생성
+    private static final int NOTIFICATION_ID = 0;
+
+
+
+    private static final String TAG = "FMS";
+    static RequestQueue requestQueue;
+    private static final String TAG2 = "MyApp";
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+
+                        // Log and toast
+                        @SuppressLint({"StringFormatInvalid", "LocalSuppress"})
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        createNotificationChannel();
+
+
+
+//=====================================웹뷰==============================================
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());  // 새 창 띄우기 않기
+        webView.setWebChromeClient(new WebChromeClient());
+        //webView.setDownloadListener(new DownloadListener(){...});  // 파일 다운로드 설정
+
+        webView.getSettings().setLoadWithOverviewMode(true);  // WebView 화면크기에 맞추도록 설정 - setUseWideViewPort 와 같이 써야함
+        webView.getSettings().setUseWideViewPort(true);  // wide viewport 설정 - setLoadWithOverviewMode 와 같이 써야함
+
+        webView.getSettings().setSupportZoom(false);  // 줌 설정 여부
+        webView.getSettings().setBuiltInZoomControls(false);  // 줌 확대/축소 버튼 여부
+
+        webView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
+//        webview.addJavascriptInterface(new AndroidBridge(), "android");
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); // javascript가 window.open()을 사용할 수 있도록 설정
+        webView.getSettings().setSupportMultipleWindows(true); // 멀티 윈도우 사용 여부
+
+        webView.getSettings().setDomStorageEnabled(true);  // 로컬 스토리지 (localStorage) 사용여부
+
+        //캐시 사용 여부
+        webView.getSettings().setAppCacheEnabled(false);
+//캐시 설정
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+// -- LOAD_CACHE_ELSE_NETWORK : 캐시 기간만료 시 네트워크 접속
+// -- LOAD_CACHE_ONLY : 캐시만 불러옴 (네트워크 사용 X)
+// -- LOAD_DEFAULT : 기본 모드, 캐시 사용, 기간 만료 시 네트워크 사용
+// -- LOAD_NO_CACHE : 캐시모드 사용안함
+// -- LOAD_NORMAL : 기본모드 캐시 사용 @Deprecated
+
+        webView.loadUrl("http://203.228.62.17");
+//===================================웹뷰==============================================
+
+//        FirebaseMessaging.getInstance().getToken()
+//                .addOnCompleteListener(new OnCompleteListener<String>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<String> task) {
+//                        if (!task.isSuccessful()) {
+//                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+//                            return;
+//                        }
+//
+//                        // Get new FCM registration token
+//                        String token = task.getResult();
+//
+//
+//                        // Log and toast
+//                        @SuppressLint({"StringFormatInvalid", "LocalSuppress"})
+//                        String msg = getString(R.string.msg_token_fmt, token);
+//                        Log.d(TAG, msg);
+//                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+
+    }
+
+    //채널을 만드는 메소드
+    public void createNotificationChannel() {
+        //notification manager 생성
+        mNotificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        // 기기(device)의 SDK 버전 확인 ( SDK 26 버전 이상인지 - VERSION_CODES.O = 26)
+        if(android.os.Build.VERSION.SDK_INT
+                >= android.os.Build.VERSION_CODES.O){
+            //Channel 정의 생성자( construct 이용 )
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID
+                    ,"Test Notification",mNotificationManager.IMPORTANCE_HIGH);
+            //Channel에 대한 기본 설정
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            // Manager을 이용하여 Channel 생성
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+    }
+    // Notification Builder를 만드는 메소드
+    private NotificationCompat.Builder getNotificationBuilder(String contents) {
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("메세지 알림")
+                .setContentText(contents)
+                .setSmallIcon(R.drawable.ic_android);
+        return notifyBuilder;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent){
+
+        if (intent != null){
+            processIntent(intent);
+        }
+        super.onNewIntent(intent);
+    }
+
+    private void processIntent(Intent intent){
+        String from = intent.getStringExtra("from");
+        if (from == null){
+
+            return;
+        }
+        String contents = intent.getStringExtra("contents");
+
+
+        sendNotification(contents);
+    }
+    public void sendNotification(String contents){
+        // Builder 생성
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder(contents);
+        // Manager를 통해 notification 디바이스로 전달
+        mNotificationManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Log.i(TAG2, "백버튼을 눌렀어요");
+        if(webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/posco/samplepush/MyFirebaseMessagingService.java
@@ -0,0 +1,153 @@
+package com.posco.samplepush;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.IBinder;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.Log;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FMS";
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onNewToken(String token){   //토큰을 서버로 전송
+        super.onNewToken(token);
+        Log.e(TAG, "onNewToken 호출됨" + token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){     //수신한 메세지를 처리
+        Log.d(TAG, "onMessageReceived() 호출됨.");
+
+        String from = remoteMessage.getFrom();
+        Map<String, String> data = remoteMessage.getData();
+        String contents = data.get("contents");
+
+        Log.d(TAG, "from : " + from + ", contents : " + contents);
+        sendToActivity(getApplicationContext(), from, contents);
+    }
+    private void sendToActivity(Context context, String from, String contents){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra("from", from);
+        intent.putExtra("contents", contents);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        context.startActivity(intent);
+    }
+}
+
+
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        if (remoteMessage.getNotification() != null) {                      //포어그라운드
+//            sendNotification(remoteMessage.getNotification().getBody(), remoteMessage.getNotification().getTitle());
+//        }else if (remoteMessage.getData().size() > 0) {                           //백그라운드
+//            sendNotification(remoteMessage.getData().get("body"), remoteMessage.getData().get("title"));
+//            /* 백그라운드 작동 내용 */
+//        }
+//
+//
+//    }
+//
+//    private void sendNotification(String messageBody, String messageTitle)  {
+//        Log.d("FCM Log", "알림 메시지: " + messageBody);
+//
+//        /* 알림의 탭 작업 설정 */
+//        Intent intent = new Intent(this, MainActivity.class);
+//
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//        String channelId = "Channel ID";
+//        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//
+//
+//        /* 알림 만들기 */
+//        NotificationCompat.Builder notificationBuilder =
+//                new NotificationCompat.Builder(this, channelId)
+//                        .setSmallIcon(R.mipmap.ic_launcher)
+//                        .setContentTitle(messageTitle)
+//                        .setContentText(messageBody)
+//                        .setAutoCancel(true)
+//                        .setFullScreenIntent(pendingIntent, true);
+//        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//
+//        /* 새로운 인텐트로 앱 열기 */
+////        Intent newintent = new Intent(this, MainActivity.class);
+////        newintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+////        startActivity( newintent );
+//
+//        /* 채널 만들기*/
+//        /* Android 8.0 이상에서 알림을 게시하려면 알림을 만들어야 함 */
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            String channelName = "Channel Name";
+//            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//
+//        notificationManager.notify(0, notificationBuilder.build());
+//    }
+//}
+
+
+
+//    private static final String TAG = "FMS";
+//    public MyFirebaseMessagingService() {
+//    }
+//
+//    @Override
+//    public void onNewToken(String token){
+//        super.onNewToken(token);
+//        Log.e(TAG, "onNewToken 호출됨" + token);
+//    }
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage){
+//        Log.d(TAG, "onMessageReceived() 호출됨.");
+//
+//        String from = remoteMessage.getFrom();
+//        Map<String, String> data = remoteMessage.getData();
+//        String contents = data.get("contents");
+//
+//        Log.d(TAG, "from : " + from + ", contents : " + contents);
+//        sendToActivity(getApplicationContext(), from, contents);
+//    }
+//    private void sendToActivity(Context context, String from, String contents){
+//        Intent intent = new Intent(context, MainActivity.class);
+//        intent.putExtra("from", from);
+//        intent.putExtra("contents", contents);
+//
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//
+//        context.startActivity(intent);
+//    }
+//}
